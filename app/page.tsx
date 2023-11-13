6f287cef5345a8a2b70f4c2be42f658b196c1b71
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-11-12 21:01:06 -0500

    Remove Welcome component and update Header component layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 // app/page.tsx
-import Image from "next/image";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { Welcome } from "@/components/Welcome";
 import { Listing } from "@/components/Listing";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-6 md:px-12 lg:px-16 xl:px-24 py-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-6 md:px-12 lg:px-16">
       <Header />
-      <Welcome />
       <Listing />
       <Footer />
     </main>
--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,41 @@
 // components/header.tsx
 import * as React from "react";
 import strings from "@/lib/strings";
+import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="flex flex-col items-center justify-center p-4 text-center">
-      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
-        {strings.header.siteTitle}
-      </h1>
-      <h2 className="text-3xl font-semibold tracking-tight border-b pb-2">
-        {strings.header.siteSubtitle}
-      </h2>
+    <header className="text-center relative mx-[-24px] md:mx-[-48px] lg:mx-[-64px] xl:mx-[-96px] mt-0">
+      <div className="absolute inset-0">
+        <Image
+          src="/hero.png"
+          alt="Hero Background"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+      <div className="relative z-10 p-4">
+        <div className="bg-black bg-opacity-50 p-4 rounded mb-4 inline-block">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-white">
+            {strings.header.siteTitle}
+          </h1>
+          <h2 className="text-3xl font-semibold tracking-tight border-b pb-2 text-white">
+            {strings.header.siteSubtitle}
+          </h2>
+        </div>
+        <div className="bg-black bg-opacity-50 p-4 rounded inline-block max-w-2xl mx-auto">
+          <h3 className="text-2xl font-semibold tracking-tight mb-4 text-white">
+            {strings.welcomeSection.heading}
+          </h3>
+          <p className="leading-7 mb-6 text-white">
+            {strings.welcomeSection.introduction}
+          </p>
+          <p className="leading-7 text-white">
+            {strings.welcomeSection.content}
+          </p>
+        </div>
+      </div>
     </header>
   );
 }
