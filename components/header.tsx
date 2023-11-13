3b75394cbaa8f80966a11d96b581e04c70a7f51a
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-11-13 02:34:48 -0500

    Refactor header component and update strings

--- components/header.tsx
+++ components/header.tsx
@@ -2,36 +2,27 @@
 import * as React from "react";
 import strings from "@/lib/strings";
 import Image from "next/image";
+import heroImage from "@/public/hero.png";
 
 export function Header() {
   return (
-    <header className="text-center relative block w-full mt-0">
-      <div className="absolute inset-0">
-        <Image
-          src="/hero.png"
-          alt="Hero Background"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-        />
-      </div>
-      <div className="relative z-10 p-4">
+    <header className="text-center relative block w-full mt-0 hero-image">
+      <div className="relative z-10 p-4 text-container">
         <div className="mb-4 inline-block">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-white text-shadow">
+          <h1 className="text-5xl font-extrabold tracking-tight lg:text-5xl text-white text-shadow">
             {strings.header.siteTitle}
           </h1>
-          <h2 className="text-3xl font-semibold tracking-tight border-b pb-2 text-white text-shadow">
+          <h2 className="text-2xl font-semibold tracking-tight pb-2 text-white text-shadow">
+            {strings.header.siteAuthor}
+          </h2>
+          <h2 className="text-2xl font-semibold tracking-tight border-b text-white text-shadow">
             {strings.header.siteSubtitle}
           </h2>
         </div>
-        <div className="bg-black bg-opacity-60 p-4 rounded block max-w-2xl mx-auto shadow-lg">
-          <h3 className="text-2xl font-semibold tracking-tight mb-4 text-white">
+        <div className="bg-black bg-opacity-70 p-4 rounded block max-w-2xl mx-auto shadow-lg">
+          <h3 className="text-3xl font-semibold tracking-tight text-white">
             {strings.welcomeSection.heading}
           </h3>
-          <p className="leading-7 text-white">
-            {strings.welcomeSection.introduction}
-          </p>
-
           <div className="sparkle-divider">
             <span>✨</span>
             <span>✨</span>
