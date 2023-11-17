ad081f83a7458672820bf8c227a26b126c820608
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-11-17 03:53:12 -0500

    Add header and footer components to time page

--- app/time/page.tsx
+++ app/time/page.tsx
@@ -1,11 +1,15 @@
 // app/time/page.tsx
 import Layout from "@/app/layout";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
 import { Time } from "@/components/Time";
 
 export const TimePage = () => {
   return (
     <Layout>
+      <Header />
       <Time />
+      <Footer />
     </Layout>
   );
 };
