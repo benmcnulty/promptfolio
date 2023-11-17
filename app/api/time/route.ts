ba8fec2feb9185f3d9d05e939b6a315a965e9b5b
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-11-17 04:23:05 -0500

    Add message field to API response

--- app/api/time/route.ts
+++ app/api/time/route.ts
@@ -3,7 +3,8 @@ export const runtime = "edge";
 
 export async function GET() {
   const serverTime = new Date().toISOString();
-  return new Response(JSON.stringify({ time: serverTime }), {
+  const message = "Hello from the edge!";
+  return new Response(JSON.stringify({ time: serverTime, message: message }), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
