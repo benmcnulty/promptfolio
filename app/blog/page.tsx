3d26174a78722c763bf74b9d41370ca3762bcc85
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-12-16 21:05:12 -0500

    Add about, blog, and listing pages

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+// app/privacy/PrivacyPolicyPage.tsx
+import React from "react";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+export default function BlogPage() {
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-between">
+      <Header />
+      <Footer />
+    </main>
+  );
+}
