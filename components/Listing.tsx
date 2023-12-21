8b0fecc1bba9c6c7babf1ff8d6ec13294131f2a4
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-12-20 21:16:00 -0500

    Refactor grid layout in Listing and Persona components

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -68,7 +68,7 @@ export function Listing() {
         {heading}
       </h3>
       <LabelToggle />
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full transition-all">
+      <div className="flex flex-wrap justify-center gap-6 w-full transition-all">
         {filteredCatalog.map((persona, index) => (
           <Persona key={index} personaData={persona} />
         ))}
--- components/Persona.tsx
+++ components/Persona.tsx
@@ -17,7 +17,7 @@ interface PersonaProps {
 
 export function Persona({ personaData }: PersonaProps) {
   return (
-    <div className="flex flex-col justify-between w-full max-w-md mx-auto my-0 bg-white rounded-lg shadow-md overflow-hidden">
+    <div className="flex flex-col justify-between w-full max-w-md mx-auto my-0 bg-white rounded-lg shadow-md overflow-hidden flex-auto md:w-1/2 lg:w-1/3 xl:w-1/4">
       <div className="relative w-full">
         <Image
           alt={personaData.alt}
