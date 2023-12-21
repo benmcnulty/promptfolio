3c5f7dd2157df280961593ec700aa682d2e6f5cf
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-12-21 15:27:48 -0500

    Refactor Welcome component and update welcome message

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -4,8 +4,8 @@ import strings from "@/lib/strings";
 export function Welcome() {
   return (
     <section className="flex flex-col items-center p-6 text-center radial-gradient-bg w-full">
-      <div className="bg-background p-4 rounded block max-w-2xl mx-auto welcome-shadow">
-        <h3 className="text-3xl font-semibold tracking-tight text-foreground">
+      <div className="bg-background bg-opacity-50 py-4 px-8 rounded block max-w-2xl mx-auto welcome-shadow">
+        <h3 className="text-3xl font-semibold tracking-tight text-foreground mb-2">
           {strings.welcomeSection.heading}
         </h3>
         <p className="leading-7 text-foreground">
--- lib/strings.ts
+++ lib/strings.ts
@@ -15,7 +15,7 @@ const strings = {
   welcomeSection: {
     heading: "Welcome to Promptfolio!",
     content:
-      "Explore the realm of AI Prompt Crafting, Engineering & Applications at Promptfolio.dev. My passion for innovation drives continuous updates and fresh creations. Dive into the latest iterations and discover new wonders with each visit. Excited to share this journey with you!",
+      "Explore my practice in AI Prompt Crafting and Engineering at Promptfolio.dev. My experience combines innovation in interaction design and software engineering with the latest in Custom GPTs, offering new insights with each visit. Join me in exploring the evolving world of AI applications.",
   },
   listing: {
     heading: "Custom GPTs for Your Software Development Needs",
