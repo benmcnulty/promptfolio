325832bc35bfc0c11d49b468d6b65e128d71f84b
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-12-21 15:04:36 -0500

    Add FocusListing component to Home page

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,11 +2,18 @@
 import { Welcome } from "./Welcome";
 import { FeaturedListings } from "./FeaturedListings";
 import { CustomListings } from "./CustomListings";
+import { FocusListing } from "./FocusListing";
 
 export function Home() {
   return (
     <section className="flex flex-col items-center text-center w-full">
       <Welcome />
+      <FocusListing
+        gptName="Prompt Mentor"
+        subheading="Start your professional prompt crafting journey!"
+        additionalContent="Let Prompt Mentor guide you along your career path from software into prompt engineering. Whether you're a student, a recent graduate, or a seasoned professional, Prompt Mentor is here to help you find your way."
+        backgroundClass="background"
+      />
       <CustomListings
         gptNames={["Prompt Mentor", "Prompt Engineer"]}
         headline="Start Prompting!"
