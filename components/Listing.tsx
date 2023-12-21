2695ae8c4fa6b10d3f6fced35d6e1b22364e116f
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-12-20 19:39:49 -0500

    Add LabelToggle component to Listing

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -7,6 +7,7 @@ import { Persona } from "@/components/Persona";
 import filterCatalog from "@/utils/filterCatalog";
 import catalog from "@/lib/catalog";
 import CatalogItem from "@/lib/types";
+import { LabelToggle } from "./LabelToggle";
 
 export function Listing() {
   const [filteredCatalog, setFilteredCatalog] =
@@ -14,22 +15,22 @@ export function Listing() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Client-side URL processing
+    // Client-side URL processing for multiple filters
     if (typeof window !== "undefined") {
       const initialFilter = searchParams.get("filter");
+      const filters = initialFilter ? initialFilter.split(",") : [];
       setFilteredCatalog(
-        initialFilter ? filterCatalog(catalog, [initialFilter]) : catalog
+        filters.length > 0 ? filterCatalog(catalog, filters) : catalog
       );
     }
   }, [searchParams]);
-
-  // Add label toggle UI component here
 
   return (
     <section className="flex flex-col items-center p-6 text-center max-w-7xl mx-auto">
       <h3 className="text-3xl font-semibold tracking-tight mb-6">
         {strings.listing.heading}
       </h3>
+      <LabelToggle />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full">
         {filteredCatalog.map((persona, index) => (
           <Persona key={index} personaData={persona} />
