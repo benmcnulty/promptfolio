6b33fe49a4b6f4313a8ca4adc1412f8970b80ccd
Ben McNulty <benmcnulty@users.noreply.github.com>
2023-12-20 20:04:21 -0500

    Refactor Listing component to dynamically set heading based on filters

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -2,7 +2,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import strings from "@/lib/strings";
 import { Persona } from "@/components/Persona";
 import filterCatalog from "@/utils/filterCatalog";
 import catalog from "@/lib/catalog";
@@ -12,24 +11,60 @@ import { LabelToggle } from "./LabelToggle";
 export function Listing() {
   const [filteredCatalog, setFilteredCatalog] =
     useState<CatalogItem[]>(catalog);
+  const [heading, setHeading] = useState("All Custom GPTs");
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Client-side URL processing for multiple filters
     if (typeof window !== "undefined") {
       const initialFilter = searchParams.get("filter");
       const filters = initialFilter ? initialFilter.split(",") : [];
       setFilteredCatalog(
         filters.length > 0 ? filterCatalog(catalog, filters) : catalog
       );
+
+      // Dynamically set the heading based on filters
+      if (filters.length > 0) {
+        const formattedFilters = filters.map(
+          (filter) => filter.charAt(0).toUpperCase() + filter.slice(1)
+        );
+
+        let headingText = "";
+
+        if (formattedFilters.includes("Featured")) {
+          headingText = "Featured GPTs";
+          const nonFeaturedFilters = formattedFilters.filter(
+            (filter) => filter !== "Featured"
+          );
+
+          if (nonFeaturedFilters.length > 0) {
+            headingText += " and GPTs for ";
+            headingText += nonFeaturedFilters.slice(0, -1).join(", ");
+            if (nonFeaturedFilters.length > 1) {
+              headingText += " & " + nonFeaturedFilters.slice(-1);
+            } else {
+              headingText += nonFeaturedFilters[0];
+            }
+          }
+        } else {
+          headingText = "GPTs for ";
+          headingText += formattedFilters.slice(0, -1).join(", ");
+          if (formattedFilters.length > 1) {
+            headingText += " & " + formattedFilters.slice(-1);
+          } else {
+            headingText += formattedFilters[0];
+          }
+        }
+
+        setHeading(headingText);
+      } else {
+        setHeading("Full List of GPTs");
+      }
     }
   }, [searchParams]);
 
   return (
-    <section className="flex flex-col items-center p-6 text-center max-w-7xl mx-auto">
-      <h3 className="text-3xl font-semibold tracking-tight mb-6">
-        {strings.listing.heading}
-      </h3>
+    <section className="flex flex-col items-center p-4 text-center max-w-7xl mx-auto">
+      <h3 className="text-3xl font-semibold tracking-tight">{heading}</h3>
       <LabelToggle />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full">
         {filteredCatalog.map((persona, index) => (
