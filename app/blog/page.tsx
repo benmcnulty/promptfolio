572fa486a0d0474c47707972f75a4466beab5517
Ben McNulty <benmcnulty@users.noreply.github.com>
2024-04-06 18:57:26 -0400

    Refactor blog page and BlogContents component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { BlogContents } from "@/components/BlogContents";
-import fs from "fs";
-import path from "path";
 import { Metadata } from "next";
 import { FeaturedListings } from "@/components/FeaturedListings";
 export const metadata: Metadata = {
@@ -13,22 +11,10 @@ export const metadata: Metadata = {
 };
 
 export default function BlogPage() {
-  const blogDirectory = path.join(process.cwd(), "app/blog/(content)");
-  const directories = fs
-    .readdirSync(blogDirectory, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-
-  const posts = directories.map((directoryName) => {
-    const slug = directoryName;
-    const title = slug.replace(/-/g, " ");
-    return { slug, title };
-  });
-
   return (
     <main className="flex flex-col min-h-screen items-center justify-between">
       <Header />
-      <BlogContents posts={posts} />
+      <BlogContents />
       <FeaturedListings />
       <Footer />
     </main>
--- components/BlogContents.tsx
+++ components/BlogContents.tsx
@@ -1,14 +1,24 @@
-// component/BlogContents.tsx
+// components/BlogContents.tsx
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Sparkle } from "@/components/ui/sparkle";
+import fs from "fs";
+import path from "path";
 
-interface BlogContentsProps {
-  posts: { slug: string; title: string }[];
-}
+export function BlogContents() {
+  const blogDirectory = path.join(process.cwd(), "app/blog/(content)");
+  const directories = fs
+    .readdirSync(blogDirectory, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
-export function BlogContents({ posts }: BlogContentsProps) {
+  const posts = directories.map((directoryName) => {
+    const slug = directoryName;
+    const title = slug.replace(/-/g, " ");
+    return { slug, title };
+  });
+
   return (
     <section className="my-8 p-6 text-center bg-card rounded-lg shadow-md max-w-4xl mx-auto">
       <h2 className="text-3xl font-semibold mb-6">Blog Posts</h2>
