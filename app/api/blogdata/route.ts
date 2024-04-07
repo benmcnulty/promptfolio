29eddd815ebb3d9ab170a0f8ecfd0fdeff55a1bb
Ben McNulty <benmcnulty@users.noreply.github.com>
2024-04-07 16:43:34 -0400

    Refactor blog route to sort and format blog posts for API response

---
+++ app/api/blogdata/route.ts
@@ -0,0 +1,31 @@
+// app/api/blogdata/route.ts
+import blog from "@/lib/blog";
+
+export const runtime = "edge";
+
+export async function GET() {
+  // Sort blog posts in chronological order based on their publish date
+  const sortedBlogPosts = blog.sort(
+    (a, b) =>
+      new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime()
+  );
+
+  // Format the blog posts for the API response
+  const formattedBlogPosts = sortedBlogPosts.map(
+    ({ slug, title, publishDate, categories, description, imageUrl }) => ({
+      slug,
+      title,
+      publishDate,
+      categories,
+      description,
+      imageUrl,
+    })
+  );
+
+  return new Response(JSON.stringify(formattedBlogPosts), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
--- lib/blog.ts
+++ lib/blog.ts
@@ -1,6 +1,15 @@
 // lib/blog.ts
 export const blog = [
   {
+    slug: "welcome-to-promptfolio",
+    title: "Welcome to Promptfolio",
+    publishDate: "2024-01-01",
+    categories: ["welcome", "promptfolio"],
+    description:
+      "Dive deep into the essence of Promptfolio, and discover how it can revolutionize your productivity.",
+    imageUrl: "/promptfolio.png",
+  },
+  {
     slug: "hello-world",
     title: "Hello World",
     publishDate: "2024-03-10",
@@ -8,15 +17,6 @@ export const blog = [
     description:
       "An introductory post to our new blog, showcasing the basics of what you can expect.",
     imageUrl: "/brainstormer.png",
-  },
-  {
-    slug: "welcome-to-promptfolio",
-    title: "Welcome to Promptfolio",
-    publishDate: "2024-01-01",
-    categories: ["welcome", "promptfolio"],
-    description:
-      "Dive deep into the essence of Promptfolio, and discover how it can revolutionize your productivity.",
-    imageUrl: "/alchemist.png",
   },
 ];
 
