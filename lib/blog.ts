c4b9b651cd36fed662f1c33ce764e72cbb64435d
Ben McNulty <benmcnulty@users.noreply.github.com>
2024-04-07 23:23:29 -0400

    Add new blog post: "Empathy-Driven Software Development"

--- lib/blog.ts
+++ lib/blog.ts
@@ -9,6 +9,15 @@ export const blog = [
       "An introductory post to our new blog, showcasing the basics of what you can expect.",
     imageUrl: "/promptfolio.png",
   },
+  {
+    slug: "empathy-driven-software-development",
+    title: "Empathy-Driven Software Development",
+    publishDate: "2024-04-07",
+    categories: ["empathy", "software development"],
+    description:
+      "A discussion on the importance of empathy in software development and how it can improve your work.",
+    imageUrl: "/promptfolio.png",
+  },
 ];
 
 export default blog;
