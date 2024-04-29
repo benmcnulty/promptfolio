8e55d4f875fe3d09b7bf57a09d203b8be24f6c13
Ben McNulty <benmcnulty@users.noreply.github.com>
2024-04-28 20:55:42 -0400

    Update metadata and descriptions for blog posts

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,26 @@ export const metadata: Metadata = {
   },
   description: "AI Prompt Crafting, Engineering & Applications.",
   metadataBase: new URL("https://promptfolio.dev/"),
+  creator: "Ben McNulty",
+  keywords: ["prompt", "ai", "gpt", "promptfolio", "ben mcnulty"],
+  openGraph: {
+    siteName: "Promptfolio by Ben McNulty",
+    images: [
+      {
+        url: `https://www.promptfolio.dev/brainstormer.png`,
+        width: 1024,
+        height: 1024,
+      },
+      {
+        url: "https://www.promptfolio.dev/hero.png",
+        width: 1792,
+        height: 1024,
+        alt: "Welcome to Promptfolio by Ben McNulty",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
