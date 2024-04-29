e0512f3cb9a1d4dcc1b797ebb80459dae4d5be4d
Ben McNulty <benmcnulty@users.noreply.github.com>
2024-04-29 02:32:01 -0400

    Update ImageCarousel component to use dynamic sizing and add new images

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -14,17 +14,22 @@ interface ImageCarouselProps {
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
   return (
-    <div className="flex overflow-x-auto snap-x snap-mandatory space-x-4 p-4">
-      {images.map((image, index) => (
-        <div key={index} className="snap-center shrink-0">
-          <FullScreenImage
-            src={image.src}
-            alt={image.alt}
-            width={500} // Default width for non-full-screen images
-            height={300} // Default height for non-full-screen images
-          />
-        </div>
-      ))}
+    <div className="relative">
+      <div className="flex overflow-x-auto snap-x snap-mandatory p-4 gap-4 my-carousel">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="snap-center shrink-0 flex-none w-80 md:w-96 h-auto" // Dynamic sizing and fixed width at different breakpoints
+          >
+            <FullScreenImage
+              src={image.src}
+              alt={image.alt}
+              width={500} // These width and height props could be removed if not required as the CSS handles sizing
+              height={300}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
